108ab0e5b11cd707484927594e142da157e7a110
Guiga897 <guifavero10@gmail.com>
2025-09-03 18:39:02 -0300

    projeto funcionando

---
+++ projeto_01/alunos/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO aluno (nome, idade, email, curso) VALUES 
+('João Silva', 20, 'joao.silva@email.com', 'Engenharia'),
+('Maria Souza', 22, 'maria.souza@email.com', 'Direito'),
+('Carlos Pereira', 19, 'carlos.pereira@email.com', 'Medicina'),
+('Ana Oliveira', 21, 'ana.oliveira@email.com', 'Arquitetura');
